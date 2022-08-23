f81b11ba4ce7dcaf00f8b842987d48b411dc4350
Scotch <blackscotch@mail.ru>
2022-08-23 03:25:14 +0300

    Первый коммит с решенной задачей номер 9

---
+++ Exp002/Program.cs
@@ -0,0 +1,15 @@
+﻿// 11. Напишите программу, которая выводит случайное трёхзначное число 
+// и удаляет вторую цифру этого числа.
+
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine(number);
+
+int number1 = number / 100;
+int number2 = number % 10;
+
+Console.Write($"{number1}{number2}");
+
+// А нужно было так)))
+
+int result = ((number / 100) * 10 + number % 10);
---
+++ Exp003/Program.cs
@@ -0,0 +1,20 @@
+﻿// 12. Напишите программу, которая будет принимать на вход два числа и 
+// выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, 
+// то программа выводит остаток от деления.
+
+Console.WriteLine("Введите первое чсило: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе чсило: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number2 % number1 == 0)
+{
+    //int result = number2 % number1;
+    Console.Write($"Число {number2} кратное {number1}");
+}
+else
+{
+    Console.Write($"Число {number2} не кратное {number1}, остаток {number2 % number1}");
+    //Console.WriteLine("Чичла кратные");
+}
